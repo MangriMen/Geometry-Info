bc4758dee31983c5d642a988909ea68dcb2a0387
MangriMen <mangrimen@gmail.com>
2020-05-16 20:43:12 +0700

    fixed redifinition in tests

--- test/test.cpp
+++ test/test.cpp
@@ -5,30 +5,30 @@
 
 TEST(PERIMETER, PerimeterCorrect)
 {
-    Circle circle(1, make_pair(0, 0));
-    ASSERT_FLOAT_EQ(6.28, circle.CircleP);
+    Circle circle1(1, make_pair(0, 0));
+    ASSERT_FLOAT_EQ(6.2831, circle1.CircleP);
 
-    // Circle circle(0.5, make_pair(0, 0));
-    // ASSERT_FLOAT_EQ(3.14, circle.CircleP);
+    Circle circle2(0.5, make_pair(0, 0));
+    ASSERT_FLOAT_EQ(3.14, circle2.CircleP);
 
-    // Circle circle(0.008, make_pair(0, 0));
-    // ASSERT_FLOAT_EQ(0.05, circle.CircleP);
+    Circle circle3(0.008, make_pair(0, 0));
+    ASSERT_FLOAT_EQ(0.05, circle3.CircleP);
 
-    // Circle circle(6.28, make_pair(0, 0));
-    // ASSERT_FLOAT_EQ(39.45, circle.CircleP);
+    Circle circle4(6.28, make_pair(0, 0));
+    ASSERT_FLOAT_EQ(39.45, circle4.CircleP);
 }
 
 TEST(AREA, AreaCorrect)
 {
-    Circle circle(3, make_pair(0, 0));
-    ASSERT_FLOAT_EQ(28.26, circle.CircleA);
+    Circle circle5(3, make_pair(0, 0));
+    ASSERT_FLOAT_EQ(28.2743, circle5.CircleA);
 
-    // Circle circle(0.1, make_pair(0, 0));
-    // ASSERT_FLOAT_EQ(0.0314, circle.CircleA);
+    Circle circle6(0.1, make_pair(0, 0));
+    ASSERT_FLOAT_EQ(0.0314, circle6.CircleA);
 
-    // Circle circle(2, make_pair(0, 0));
-    // ASSERT_FLOAT_EQ(12.56, circle.CircleA);
+    Circle circle7(2, make_pair(0, 0));
+    ASSERT_FLOAT_EQ(12.56, circle7.CircleA);
 
-    // Circle circle(1.978, make_pair(0, 0));
-    // ASSERT_FLOAT_EQ(12.2852, circle.CircleA);
+    Circle circle8(1.978, make_pair(0, 0));
+    ASSERT_FLOAT_EQ(12.2852, circle8.CircleA);
 }
